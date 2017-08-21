c53021b14c947e0f42dbf8f16ae45b0e175548bd
Pravin Kumar S <pravinit2@gmail.com>
2017-08-21 18:01:37 +0530

    added side bar styles

--- app.js
+++ app.js
@@ -4,38 +4,34 @@ app.controller("initController", function ($scope) {
 });
 app.controller("sidenavController", function ($scope) {
     var imagePath = 'https://material.angularjs.org/latest/img/list/60.jpeg';
+    $scope.search = "";
     $scope.messages = [{
-      face : imagePath,
-      what: 'Rapidcare IT',
-      who: 'Ram :',
-      when: '3:08PM',
-      notes: " Good morning"
+        face: imagePath,
+        user: 'Ram',
+        when: '3:08PM',
+        notes: "Good morning"
     }, {
-      face : imagePath,
-      what: 'Pravin IT',
-      who: 'You :',
-      when: '3:08PM',
-      notes: "Bye"
+        face: imagePath,
+        user: 'Pravin',
+        when: '3:08PM',
+        notes: "Bye"
     }, {
-      face : imagePath,
-      what: 'RC suresh',
-      who: 'You :',
-      when: '3:08PM',
-      notes: "Dai"
+        face: imagePath,
+        user: 'Kumar',
+        when: '3:08PM',
+        notes: "Dai"
     }, {
-      face : imagePath,
-      what: 'Dhinesh',
-      who: 'You :',
-      when: '3:08PM',
-      notes: "OK"
+        face: imagePath,
+        user: 'Ashok',
+        when: '3:08PM',
+        notes: "OK"
     }, {
-      face : imagePath,
-      what: 'IT GUYS',
-      who: 'Ram :',
-      when: '3:08PM',
-      notes: " I'll be"
+        face: imagePath,
+        user: 'Dinesh',
+        when: '3:08PM',
+        notes: " I'll be"
     }];
-  });
+});
 
 
   

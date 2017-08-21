bd6a019dc934fd457c2d3288cd649a3222daad86
cristianomessi <ram3green@gmail.com>
2017-08-21 15:54:43 +0530

    chat list

--- app.js
+++ app.js
@@ -3,5 +3,39 @@ app.controller("initController", function ($scope) {
     $scope.appName = "NG-CHAT";
 });
 app.controller("sidenavController", function ($scope) {
-    $scope.menuList = ["pravin", "lavanya", "ashok", "kumar"];
-});
+    var imagePath = 'https://material.angularjs.org/latest/img/list/60.jpeg';
+    $scope.messages = [{
+      face : imagePath,
+      what: 'Rapidcare IT',
+      who: 'Ram :',
+      when: '3:08PM',
+      notes: " Good morning"
+    }, {
+      face : imagePath,
+      what: 'Pravin IT',
+      who: 'You :',
+      when: '3:08PM',
+      notes: "Bye"
+    }, {
+      face : imagePath,
+      what: 'RC suresh',
+      who: 'You :',
+      when: '3:08PM',
+      notes: "Dai"
+    }, {
+      face : imagePath,
+      what: 'Dhinesh',
+      who: 'You :',
+      when: '3:08PM',
+      notes: "OK"
+    }, {
+      face : imagePath,
+      what: 'IT GUYS',
+      who: 'Ram :',
+      when: '3:08PM',
+      notes: " I'll be"
+    }];
+  });
+
+
+  
